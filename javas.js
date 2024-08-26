8c12919f56ef11845f6c34337060812c76ae714b
Code-create9876 <shraddha.agrawal.sa9876@gmail.com>
2024-08-26 13:27:36 +0530

    Add files via upload

---
+++ javas.js
@@ -0,0 +1,18 @@
+var i = 0;
+var txt1 =
+  "Hi Love.....!  <<               Now I wan't to say something special to you. <<<                So , Please read everything carefully...!                                                                           > When I saw you for the first time < You seems something Special to me.  <<                  As the days goes < you get closer to me....! <<                           I don't know the reason why your thoughts always resonates inside my mind...!                                                     > Everything about you is always intresting for me...!                                                                                             << Now You are the only person  whom I am this much Comfortable,babe I LOVE YOU                                                            >I Love U <SweetHeart.....!                  > Maan jao nhi to bta rhi hu kosi se uthwa liye jaoge....! |";
+var speed = 55;
+typeWriter();
+function typeWriter() {
+  if (i < txt1.length) {
+    if (txt1.charAt(i) == "<")
+      document.getElementById("text1").innerHTML += "</br>";
+    else if (txt1.charAt(i) == ">")
+      document.getElementById("text1").innerHTML = "";
+    else if (txt1.charAt(i) == "|") {
+      $(".bg_heart").css("background-image", "')");
+    } else document.getElementById("text1").innerHTML += txt1.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
